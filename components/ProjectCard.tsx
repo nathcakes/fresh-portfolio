47f8a34d28fa127a38959837ddee79d27ca7f297
Nate Brown <nate@nateb.dev>
2023-08-12 22:49:25 +0800

    started working on the project cards from the figma design

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,15 +2,20 @@
 export default function ProjectCard(props: any, children: string){
     return(
     <>
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes"/></figure>
-        <div className="card-body">
-            <h2 className="card-title">{props.title}</h2>
-            <p>{props.children}</p>
+    <div className="card card-compact w-124 bg-base-200 shadow-xl flex">
+        <picture><img src="/glitchy-deno.jpg" className={"object-cover rounded-full h-24 w-24"} alt={props.title + " project teaser image."}/></picture>
+        <a href={props.ghlink} title={"Check out " + props.title + " on Github."}>
+            <picture><img src="/github.svg" className={"object-cover rounded-full h-12 w-12"} alt="Github Icon"/></picture>
+        </a>
+        <a href={props.link} title={"Check out " + props.title + " in action."}>
+            <picture><img src="/link-icon.svg" className={"object-cover rounded-full h-12 w-12"} alt="Link Icon"/></picture>
+        </a>
+        <div class={"text-l text-white text-center md:text-justify"}>
+            <h2 className="card-title text-xl font-bold">Temp Title replace with {props.title}</h2>
+            <p>Filler content replace with {props.children}
+                Minim anim ut ipsum labore non elit duis occaecat non. Minim anim ut ipsum labore non elit duis occaecat non.</p>
             <a href={props.link}>
-            <div className="card-actions justify-end">
-                <button className="btn btn-primary">Check It Out</button>
-            </div>
+
             </a>
         </div>
     </div>
--- routes/projects.tsx
+++ routes/projects.tsx
@@ -1,19 +1,7 @@
 import {NavBar} from '/components/NavBar.tsx';
 import { Container } from "/components/Container.tsx";
 import { Head } from "$fresh/runtime.ts"
-// import MotionCard from "/islands/MotionCard.tsx";
-
-// function Card(card:project){
-//     return (
-//         <div class={"flex flex-col justify-center items-center"}>
-//             <div class={"flex flex-col justify-center items-center bg-[#242424] p-4 rounded-md"}>
-//                 <h3 class={"text-2xl font-bold"}>{card.title}</h3>
-//                 <p class={"text-center"}>{card.description}</p>
-//                 <a href={card.link} class={"text-blue-500"}>Check it out</a>
-//             </div>
-//         </div>
-//     );
-// }
+import ProjectCard from "/components/ProjectCard.tsx";
 
 function Card({title, description}: {title: string, description: string}){
     return (
@@ -26,35 +14,6 @@ function Card({title, description}: {title: string, description: string}){
         </div>
     );
 }
-
-// class project {
-//     title: string;
-//     description: string;
-//     link: string;
-//     constructor(title: string, description: string, link: string){
-//         this.title = title;
-//         this.description = description;
-//         this.link = link;
-//     }
-// }
-// const Carousel = (card:project) => {
-//     return(<>
-//     <AnimatePresence mode={"popLayout"}>
-//         <motion.div key={card}
-//                     initial={{x: -300}}
-//                     anime={{x:0}}
-//                     exit={{x:300}}
-//         >
-//             {Card(card)}
-//         </motion.div>
-//     </AnimatePresence>
-//         </>
-//     );
-// }
-//        <main class={"max-w-screen-md px-4 pt-16 mx-auto mt-20"}>
-//             <MotionCard title={"test"} description={"name"} link={"stuff"}/>
-//
-//         </main>
 
 
 export default function ProjectsPage(){
@@ -71,6 +30,7 @@ export default function ProjectsPage(){
                 <Card  title={"I'm Still Working On Making This Page Fantastic"} description={"In the meantime, please checkout my github for some projects I've worked on."}/>
                 </a>
             </div>
+            <ProjectCard/>
         </Container>
     );
 }
