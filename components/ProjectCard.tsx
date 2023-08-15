9f093f7b126be6f782c0317b2854828a717197c2
Nate Brown <nate@nateb.dev>
2023-08-15 19:20:00 +0800

    finished the new projects page

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,8 +2,8 @@
 export function ProjectCard(props: any, children: string){
     return(
     <>
-    <div className="card card-compact bg-[#242424] rounded-xl m-5 flex flex-row-reverse lg:flex-row flex-nowrap">
-        <div className={"space-y-36 mr-4 w-1/4"}>
+    <div className="card card-compact max-w-screen-md bg-[#242424] rounded-xl m-5 flex flex-row-reverse lg:flex-row flex-nowrap">
+        <div className={"space-y-20 mr-4 w-1/4"}>
         <picture className={"flex items-start "}><img src={props.source} className={"rounded-full pt-5 pl-5 h-auto w-full"} alt={props.title + " project teaser image."}/></picture>
         <div className={"flex flex-col-reverse items-end sm:hidden"}>
 
@@ -15,7 +15,7 @@ export function ProjectCard(props: any, children: string){
             </a>
         </div>
         </div>
-        <div class={"text-l text-white  md:text-justify p-2 m-4 w-3/4"}>
+        <div class={"text-l text-white max-w-prose md:text-justify p-2 m-4 w-3/4"}>
             <h2 className="flex flex-nowrap card-title text-3xl font-bold">{props.title}</h2>
             <p className={"pt-4"}>{props.children}</p>
             <div className={"hidden sm:flex flex-row-reverse md:mt-2"}>
@@ -38,11 +38,11 @@ export function ProjectCard(props: any, children: string){
 export function InProgressProjectCard(props: any, children: string){
     return(
         <>
-            <div className="card card-compact bg-[#242424] rounded-xl m-5 flex flex-row-reverse lg:flex-row flex-nowrap">
-                <div className={"space-y-36 mr-4 w-1/4"}>
+            <div className="card card-compact max-w-screen-md bg-[#242424] rounded-xl m-5 flex flex-row-reverse lg:flex-row flex-nowrap">
+                <div className={"space-y-24 mr-4 w-1/4"}>
                     <picture className={"flex items-start "}><img src={props.source} className={"rounded-full pt-5 pl-5 h-auto w-full"} alt={props.title + " project teaser image."}/></picture>
                 </div>
-                <div class={"text-l text-white  md:text-justify p-2 m-4 w-3/4"}>
+                <div class={"text-l text-white max-w-prose md:text-justify p-2 m-4 w-3/4"}>
                     <h2 className="flex flex-nowrap card-title text-3xl font-bold">{props.title}</h2>
                     <p className={"pt-4"}>{props.children}</p>
                 </div>
--- routes/projects.tsx
+++ routes/projects.tsx
@@ -12,14 +12,14 @@ export default function ProjectsPage(){
             <Head><title>Nate's Projects</title>
             </Head>
             <NavBar />
-            <div class={"max-w-screen-7xl px-4 pt-16 mt-16"}>
-                <div class={"flex flex-row flex-wrap lg:flex-nowrap "}>
-                    <ProjectCard class={"flex-1"} source={"/copilot-theme.png"} title={"IntelliJ Copilot Dark Theme"} link={"https://plugins.jetbrains.com/plugin/19537-copilot-dark-theme"}
+            <div class={"max-w-fit px-4 pt-16 mt-16"}>
+                <div class={"flex flex-row justify-center flex-wrap lg:flex-nowrap "}>
+                    <ProjectCard class={"max-w-prose"} source={"/copilot-theme.png"} title={"IntelliJ Copilot Dark Theme"} link={"https://plugins.jetbrains.com/plugin/19537-copilot-dark-theme"}
                                  ghlink={"https://github.com/nathcakes/Copilot-Dark-Theme-IntelliJ"}>
                         Inspired by the colours from Github's original Co-Pilot announcement page and a <a href={"https://marketplace.visualstudio.com/items?itemName=BenjaminBenais.copilot-theme"}>VSCode Extension</a>  of the same name,
                         I created my first IntelliJ theme. I had to refer to developer docs and use IntelliJ's element inspector to get details on new UI elements that hadn't been documented yet.
                     </ProjectCard>
-                    <InProgressProjectCard source={"/ml-icon.png"} class={"flex-1"} title={"NLP for Diabetes Diagnosis"}>
+                    <InProgressProjectCard source={"/ml-icon.png"} class={"max-w-prose"} title={"NLP for Diabetes Diagnosis"}>
                         I'm currently working on a Natural Language Processing algorithm to diagnose diabetes from a patient's medical history. This work is being completed as an undergraduate research project under the supervision of Dr. Md Zakir Hossain.
                         Upon completion and marking, the code will be open sourced on github and I will link to my project report here.
                     </InProgressProjectCard>
